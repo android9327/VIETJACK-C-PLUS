316ab283266bfadbf64dea2e2169e548cb2ace1d
android9327 <34136405+android9327@users.noreply.github.com>
2018-01-09 00:37:40 +0700

    Add files via upload
    Add header file ...

---
+++ bai1.11.1/addHeader/addHeader/Header1.h
@@ -0,0 +1,4 @@
+#pragma once
+#define NAME "Hong Thai"
+#define AGE 23
+#define SEX "MALE"
---
+++ bai1.11.1/addHeader/addHeader/addHeader.cpp
@@ -0,0 +1,16 @@
+// addHeader.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "Header1.h"
+
+using namespace std;
+int main()
+{
+	cout << "name: " << NAME << endl;
+	cout << "age:  " << AGE << endl;
+	cout << "sex:  " << SEX << endl;
+		return 0;
+}
+
